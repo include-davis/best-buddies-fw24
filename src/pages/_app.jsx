c9e5298d856a930afd5185e7b7dc97cd96fe48e3
Ryan <ryyu444@gmail.com>
2024-01-27 15:19:35 -0800

    added font fallback & update font files

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,20 +3,22 @@ import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
 import "@/styles/globals.scss";
 
+// note: weight & style property determines the font used?
 const helveticaNeue = localFont({
   src: [
     {
-      path: "../fonts/HelveticaNeue.ttf",
+      path: "../fonts/HelveticaNeue.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/HelveticaNeueBold.ttf",
+      path: "../fonts/HelveticaNeueBold.otf",
       weight: "700",
       style: "bold",
     },
   ],
   variable: "--font-helvetica-neue",
+  fallback: ["arial"],
 });
 
 export default function App({ Component, pageProps }) {
