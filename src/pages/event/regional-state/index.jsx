d4b16c3eaff57d2dd399361571b3b0a17542c518
erikagosti <erika.gosti@gmail.com>
2024-02-01 17:14:04 -0800

    updated state events title/friendship walk

--- src/pages/event/regional-state/index.jsx
+++ src/pages/event/regional-state/index.jsx
@@ -3,7 +3,31 @@ import styles from "@/styles/pages/event/regional-state.module.scss";
 export default function RegionalStateEvents() {
   return (
     <div>
-      <h1 className={styles.regional-state}>Regional State Events</h1>
+      <div>
+        <h1 className={styles.regional_state_title}>Regional State Events</h1>
+      </div>
+      <div className={styles.friendship_walk_card}>
+        <div className={styles.friendship_walk}>
+          <p>
+            The Best Buddies Friendship Walk is the leading walk in the 
+            country supporting inclusion for people with IDD. Over 60 U.S. 
+            cities host Friendship Walks each year, and over 150,000 
+            participants have walked to support inclusion since 2009. The 
+            Friendship Walk is free to attend (although fundraising is 
+            encouraged!) and family-friendly.
+          </p>
+          <p>
+          It&#39;s not just a walk &#8208; Friendship Walk attendees also get to 
+          enjoy entertainment including song and dance performances, 
+          ice cream trucks, and lawn games. Funds raised at the 
+          Friendship Walk are used to maintain and expand local Best 
+          Buddies programs.
+          </p>
+        </div>
+        <div>
+          <img />
+        </div>
+      </div>
     </div>
   );
 }
