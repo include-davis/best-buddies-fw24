bdc3ec3c69d1eaaf3fd8fad39305af54ebd98995
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2024-02-07 19:25:56 -0800

    link tag comment

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@ import styles from "@/styles/components/button/button.module.scss";
 export default function Button(props){
     return (
         <a href={`/${props.pageName}`} className={styles.buttonLink}>
-            <button className={styles.button}>{props.children}</button>
+            <button className={styles.button}>{props.children}</button> {/* use next js link tag */}
         </a>
     );
 }
