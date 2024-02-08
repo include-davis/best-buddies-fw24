eee279a1b157345433c5ba4cb8969fac6eb75e99
catelynbu <catelyn.bu@gmail.com>
2024-02-07 18:29:55 -0800

    some small changes, building css

--- src/pages/about/intl/index.jsx
+++ src/pages/about/intl/index.jsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/pages/about/about-intl.module.scss";
 
+
 export default function AboutIntl() {
   return (
     <div className={styles.body}>
@@ -14,44 +15,52 @@ export default function AboutIntl() {
         {/*shapes*/}
       </div>
 
-    <div className={styles.frames}>
-      <div className={styles.friendships}>
-       {/*img goes here*/}
-        <div className={styles.frame}>
-          <h2 className={styles.h2text}>One-To-One Friendships</h2> {/*needs emoji*/}
-            <p>These programs build one-to-one friendships between people with and without intellectual and development disabilities (IDD), offering social interactions while improving the quality of life and level of inclusion for a population that is often isolated and excluded.
-            </p>
+      <div className={styles.cards}> {/*maybe*/}
+        <div className={styles.friendships}>
+        {/*img goes here*/}
+          <div className={styles.frame}>
+            <h2 className={styles.h2text}>One-To-One Friendships</h2>
+              <p>These programs build one-to-one friendships between people with and without intellectual and development disabilities (IDD), offering social interactions while improving the quality of life and level of inclusion for a population that is often isolated and excluded.
+              </p>
+          </div>
+        </div>
+
+        <div className={styles.intergratedEmployment}>
+        {/*img goes here*/}
+          <div className={styles.frame}>
+            <h2 className={styles.h2text}>Intergrated Employment</h2> 
+        
+            {/*needs emoji*/}
+              <p>This program secures jobs for people with intellectual and developmental disabilities (IDD), allowing them to earn an income, pay taxes, and continuously and independently support themselves.
+              </p>
+          </div>
+        </div>
+
+        <div className={styles.leadershipDevelopment}>
+        {/*img goes here*/}
+          <div className={styles.frame}>
+            <h2 className={styles.h2text}>Leadership Development</h2> {/*needs emoji*/}
+              <p>
+              These programs educate and empower people with and without IDD to become leaders, public speakers, and advocates. Best Buddies passionately believes that its program participants can lead the effort within their community and beyond to build a more inclusive world for people with IDD.
+              </p>
+          </div>
+        </div>
+
+        <div className={styles.inclusiveLiving}>
+        {/*img goes here*/}
+          <div className={styles.frame}>
+            <h2>Inclusive Living</h2> {/*needs emoji*/}
+              <p>
+              This program fosters an integrated experience for people with and without IDD to become active, contributing citizens who live independently in a dynamic environment where they can learn, grow, and thrive locations in Washington, D.C, Miami, FL, and Los Angeles, CA.            </p>
+          </div>
         </div>
       </div>
 
-      <div className={styles.intergratedEmployment}>
-       {/*img goes here*/}
-        <div className={styles.frame}>
-          <h2 className={styles.h2text}>Intergrated Employment</h2> {/*needs emoji*/}
-            <p>This program secures jobs for people with intellectual and developmental disabilities (IDD), allowing them to earn an income, pay taxes, and continuously and independently support themselves.
-            </p>
-        </div>
+      <div className={styles.learnMore}>
+        <h2 className={styles.learnMoreH2}>Want to learn more?</h2>
+        <p>Visit the Best Buddies International Official website.</p>
+        {/*button goes here*/}
       </div>
-
-      <div className={styles.leadershipDevelopment}>
-       {/*img goes here*/}
-        <div className={styles.frame}>
-          <h2 className={styles.h2text}>Leadership Development</h2> {/*needs emoji*/}
-            <p>
-            These programs educate and empower people with and without IDD to become leaders, public speakers, and advocates. Best Buddies passionately believes that its program participants can lead the effort within their community and beyond to build a more inclusive world for people with IDD.
-            </p>
-        </div>
-      </div>
-
-      <div className={styles.inclusiveLiving}>
-       {/*img goes here*/}
-        <div className={styles.frame}>
-          <h2>Inclusive Living</h2> {/*needs emoji*/}
-            <p>
-            This program fosters an integrated experience for people with and without IDD to become active, contributing citizens who live independently in a dynamic environment where they can learn, grow, and thrive locations in Washington, D.C, Miami, FL, and Los Angeles, CA.            </p>
-        </div>
-      </div>
-     </div>
 
     </div>
 
