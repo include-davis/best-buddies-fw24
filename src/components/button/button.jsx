9d9fbf45e35680e2eb00818cb2f203c6230b7ffd
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2024-02-17 14:55:45 -0800

    responsivity using mixins

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,13 +2,16 @@ import styles from "@/styles/components/button/button.module.scss";
 import Link from 'next/link'
 
 // usage: <Button href={"<name_of_page>"} label={"<button text>"}/>
+
 // <name_of_page> is name of the page, such as "member", "contact", etc
 // unless you are linking the official best buddies website (special case because it's not a relative path): use "official"
 
-export default function Button({label, href}){
+// the hero prop is only true for the button in the homepage hero, i.e. only naomi needs to use it and you can ignore it otherwise
+
+export default function Button({label, href, hero=false}){
     return (
         <Link href={(href=='official')? 'https://www.bestbuddies.org/' : `/${href}`} className={styles.buttonLink}>
-            <button className={styles.button}>{label}</button> 
+            <button className={(hero)? styles.heroButton : styles.button}>{label}</button> 
         </Link>
     );
 }
