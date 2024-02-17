9929515e8a0b7db6a813722f2390aa1a7cae91d4
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2024-02-17 13:51:21 -0800

    revert index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Button from "@/components/button/button"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className={`body-1`}>
-      <div>
-        Best Buddies at UC Davis is a chapter of the global nonprofit Best Buddies
-        International, which promotes social and economic inclusion for people
-        with intellectual and developmental disabilities (IDD). We envision a
-        world in which programs like Best Buddies are no longer needed because
-        people with IDD are completely included in their communities. 
-      </div>
+      Best Buddies at UC Davis is a chapter of the global nonprofit Best Buddies
+      International, which promotes social and economic inclusion for people
+      with intellectual and developmental disabilities (IDD). We envision a
+      world in which programs like Best Buddies are no longer needed because
+      people with IDD are completely included in their communities. 
     </div>
   );
 }
