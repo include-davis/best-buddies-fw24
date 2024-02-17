9068fe01e59d73e7791f8c453f5b2bfb57397f84
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2024-02-17 11:51:16 -0800

    updates for convention

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,9 +1,10 @@
 import styles from "@/styles/components/button/button.module.scss";
+import Link from 'next/link'
 
 export default function Button(props){
     return (
-        <a href={`/${props.pageName}`} className={styles.buttonLink}>
-            <button className={styles.button}>{props.children}</button> {/* use next js link tag */}
-        </a>
+        <Link href={`/${props.pageName}`} className={styles.buttonLink}>
+            <button className={styles.button}>{props.children}</button> 
+        </Link>
     );
 }
