40db18169c75cefb1346ffb33a322ef961aac685
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2024-02-17 15:19:06 -0800

    allow styling override as prop

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -6,12 +6,14 @@ import Link from 'next/link'
 // <name_of_page> is name of the page, such as "member", "contact", etc
 // unless you are linking the official best buddies website (special case because it's not a relative path): use "official"
 
-// the hero prop is only true for the button in the homepage hero, i.e. only naomi needs to use it and you can ignore it otherwise
+// optional: if your button has styles that differ from the default button component, you can pass in those styles as a prop
+// extraStyle = {styles.<className>}
+// the css for className only needs to include overrided attributes. 
 
-export default function Button({label, href, hero=false}){
+export default function Button({label, href, extraStyles=null}){
     return (
         <Link href={(href=='official')? 'https://www.bestbuddies.org/' : `/${href}`} className={styles.buttonLink}>
-            <button className={(hero)? styles.heroButton : styles.button}>{label}</button> 
+            <button className={[styles.button, extraStyles].join(' ')}>{label}</button> 
         </Link>
     );
 }
