6d028a86a0db5cd742b222810caee00b86d3adb0
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2024-02-21 05:23:02 -0800

    Moved card component html/css from chapter events + used Button component

---
+++ src/components/eventCard/eventCard.jsx
@@ -0,0 +1,18 @@
+import styles from "@/styles/components/footer/eventcard.module.scss";
+import Image from "next/image";
+import Button from "../button/button";
+
+export default function EventCard({ title, date, description, imagePath, eventLink }) {
+    return (
+      <div className={styles.card}>
+        <div className={styles.image}>
+          <Image src={imagePath} alt={title} width={352} height={240} />
+        </div>
+        <h3 className={styles.cardTitle}>{title}</h3>
+        <p className={styles.cardSubtitle}>{date}</p>
+        <p className={styles.cardDescription}>{description}</p>
+        <Button label="RSVP" href={eventLink} />
+      </div>
+    );
+  }
+  
--- src/pages/event/chapter/index.jsx
+++ src/pages/event/chapter/index.jsx
@@ -1,9 +1,74 @@
 import styles from "@/styles/pages/event/chapter.module.scss";
+import Image from 'next/image';
 
 export default function ChapterEvents() {
   return (
     <div>
-      <h1 className={styles.chapter}>Chapter Events</h1>
+      <div className={styles['featured-events']}>
+          <h1 className={styles.chapter}>Chapter Events</h1>
+        <div className={styles.box}>
+        <h2 className={styles.title}>Featured Events</h2>
+          <div className={styles.icon}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+              <path d="M34.8333 7.33337H9.16667C7.14162 7.33337 5.5 8.975 5.5 11V36.6667C5.5 38.6918 7.14162 40.3334 9.16667 40.3334H34.8333C36.8584 40.3334 38.5 38.6918 38.5 36.6667V11C38.5 8.975 36.8584 7.33337 34.8333 7.33337Z" fill="#B4DDD5" stroke="#8FD1C4" stroke-width="3.66667" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M29.3334 3.66663V11" stroke="#8FD1C4" strokeWidth="3.66667" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M14.6666 3.66663V11" stroke="#8FD1C4" strokeWidth="3.66667" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M5.5 18.3334H38.5" stroke="#8FD1C4" strokeWidth="3.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </div>
+        </div>
+        <div className={styles['cards-container']}>
+          <div className={styles.card}>
+            <div className={styles.image}>
+              <Image
+                src="/member-imgs/newMemHero.jpg"
+                alt="New Member"
+                width={352}
+                height={240} 
+              />
+            </div>
+            <h3 className={styles['card-title']}>Event Title</h3>
+            <p className={styles['card-subtitle']}>Month Day, Year</p>
+            <p className={styles['card-description']}>Event Description</p>
+            <button className={styles.button}>RSVP</button>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.image}>
+              <Image
+                  src="/member-imgs/newMemHero.jpg"
+                  alt="New Member"
+                  width={352}
+                  height={240} 
+                />
+            </div>
+            <h3 className={styles['card-title']}>Event Title</h3>
+            <p className={styles['card-subtitle']}>Month Day, Year</p>
+            <p className={styles['card-description']}>Event Description</p>
+            <button className={styles.button}>RSVP</button>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.image}>
+              <Image
+                  src="/member-imgs/newMemHero.jpg"
+                  alt="New Member"
+                  width={352}
+                  height={240} 
+                />
+            </div>
+            <h3 className={styles['card-title']}>Event Title</h3>
+            <p className={styles['card-subtitle']}>Month Day, Year</p>
+            <p className={styles['card-description']}>Event Description</p>
+            <button className={styles.button}>RSVP</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCalendar}>
+        <h2 className={styles.title}>Event Calendar</h2>
+        <p className={styles.description}>
+          Stay in the know! We'll announce upcoming events at least two weeks in advance. All events are in person unless stated otherwise. Don't forget to find the RSVP link in the event description.
+        </p>
+        <div className={styles.calendarPlaceholder}></div>
+      </div>
     </div>
   );
 }
