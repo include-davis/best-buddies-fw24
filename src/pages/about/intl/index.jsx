df38a4ad1765fdfac8ff5c294e878096ad4204da
catelynbu <catelyn.bu@gmail.com>
2024-02-21 18:33:40 -0800

    added images, restructured some html, lots of css added

--- src/pages/about/intl/index.jsx
+++ src/pages/about/intl/index.jsx
@@ -1,27 +1,64 @@
 import styles from "@/styles/pages/about/about-intl.module.scss";
+import Image from "next/image";
 
 
 export default function AboutIntl() {
   return (
     <div className={styles.body}>
 
-      <div className={styles.hero}>
-        {/*hero img goes here*/}
+      <div className={styles.heroBox}>
+        <img className={styles.heroImg}
+        src="/about-imgs/BBI.jpeg"
+        alt="Large group of members posing together in front of a banner and a purple and white ballon arch."
+        />
+
         <h1 className={styles.heroText}>Best Buddies International</h1>
       </div>
 
-      <div className={styles.pillars}>
-        <h2>Our Four Pillars</h2>
-        {/*shapes*/}
+     
+        <h2 className={styles.margin}>Our Four Pillars</h2>
+        
+   
+        <div className={styles.pillars}>
+          <div className={styles.YellowCircle}>
+            <h3>One-to-One Friendships</h3>
+          </div>
+
+          <div className={styles.GreenCircle}>
+            <h3>Intergrated Employment</h3>
+          </div>
+
+          <div className={styles.RedCircle}>
+            <h3>Leadership Development</h3>
+          </div>
+
+          <div className={styles.PurpleCircle}>
+            <h3>Inclusive Living</h3>
+          </div>
       </div>
 
       <div className={styles.cards}> {/*maybe*/}
+
+
         <div className={styles.friendships}>
-        {/*img goes here*/}
-          <div className={styles.frame}>
-            <h2 className={styles.h2text}>One-To-One Friendships</h2>
-              <p>These programs build one-to-one friendships between people with and without intellectual and development disabilities (IDD), offering social interactions while improving the quality of life and level of inclusion for a population that is often isolated and excluded.
-              </p>
+          <img className={styles.pillarImg}
+          src="/about-imgs/Friendship.jpg"
+          alt="A buddy pair, both girls, stand in front of a purple wall. They are smiling and posing together."
+          />
+
+          <div className={styles.textFrame}>
+            <div className={styles.headers}>
+              <h2>One-To-One Friendships</h2>
+
+              <img
+              src="/page-icons/heart.svg"
+              alt="Yellow heart." 
+              />
+            </div>
+
+            <p>These programs build one-to-one friendships between people with and without intellectual and development disabilities (IDD), offering social interactions while improving the quality of life and level of inclusion for a population that is often isolated and excluded.
+            </p>
+
           </div>
         </div>
 
