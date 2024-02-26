b1f0c2e7a2b59e1f81ad3d6d6049160893236a0c
catelynbu <catelyn.bu@gmail.com>
2024-02-25 23:17:39 -0800

    mapping + css changes + worked on image responsiveness

--- src/pages/about/intl/index.jsx
+++ src/pages/about/intl/index.jsx
@@ -1,10 +1,168 @@
 import styles from "@/styles/pages/about/about-intl.module.scss";
+import Image from "next/image";
+
+const intlImgs = [
+  /*{
+    title: "hero",
+    description: "",
+    path: "/about-imgs/intl/about-intl-banner.jpg",
+    altText: "Large group of members posing together in front of a banner and a purple and white ballon arch.",
+  },*/
+
+  {
+    title: "One-to-One Friendships",
+    description: "These programs build one-to-one friendships between people with and without intellectual and developmental disabilities (IDD), offering social interactions while improving the quality of life and level of inclusion for a population that is often isolated and excluded.",
+    path: "/about-imgs/intl/about-intl-friendship.jpg",
+    altText: "A buddy pair, both girls, stand in front of a purple wall. They are smiling and posing together.",
+  },
+
+  {
+    title: "Integrated Employment",
+    description: "This program secures jobs for people with intellectual and developmental disabilities (IDD), allowing them to earn an income, pay taxes, and continuously and independently support themselves.",
+    path: "/about-imgs/intl/about-intl-jobs.jpg",
+    altText: "A Best Buddies participant stands in front of a sign that reads 'John Hancock'. He is wearing a black suit, light blue shirt, and tan tie.",
+  },
+
+  {
+    title: "Leadership Development",
+    description: "These programs educate and empower people with and without IDD to become leaders, public speakers, and advocates. Best Buddies passionately believes that its program participants can lead the effort within their community and beyond to build a more inclusive world for people with IDD.",
+    path: "/about-imgs/intl/about-intl-leadership.jpg",
+    altText: "A Best Buddies Ambassador stands at a podium and speaks into a microphone. He is wearing a blue button-down shirt and tan slacks.",
+  }, 
+  
+  {
+    title: "Inclusive Living",
+    description: "This program fosters an integrated experience for people with and without IDD to become active, contributing citizens who live independently in a dynamic environment where they can learn, grow, and thrive locations in Washington, D.C, Miami, FL, and Los Angeles, CA.",
+    path: "/about-imgs/intl/about-intl-living.jpg",
+    altText: "Four Best Buddies participants are gathered around a kitchen island. One of them is slicing a tomato.",
+  },
+]
+
+const icons = [
+  {
+    title:"heart",
+    path: "/page-icons/heart.svg"
+  },
+
+  {
+    title: "briefcase",
+    path: "/page-icons/briefcase.svg"
+  },
+
+  {
+    title: "user",
+    path: "/page-icons/users.svg",
+  },
+
+  {
+    title: "smile",
+    path: "/page-icons/smile.svg",
+  }
+]
+
 
 
 export default function AboutIntl() {
   return (
     <div className={styles.body}>
 
+      <div className={styles.heroContainer}>
+        <Image className={styles.heroImg}
+        src="/about-imgs/intl/about-intl-banner.jpg"
+        alt="Large group of members posing together in front of a banner and a purple and white ballon arch."
+        objectFit="cover" 
+        objectPosition="center"
+        fill
+        />
+        <div className={styles.heroText}>
+          <h1>Best Buddies</h1>
+          <h1>International</h1>
+        </div>
+      </div>
+
+      <h2 className={styles.margin}>Our Four Pillars</h2>
+        <div className={styles.pillars}>
+          <div className={styles.YellowCircle}>
+            <h3>One-to-One Friendships</h3>
+          </div>
+
+          <div className={styles.GreenCircle}>
+            <h3>Intergrated Employment</h3>
+          </div>
+
+          <div className={styles.RedCircle}>
+            <h3>Leadership Development</h3>
+          </div>
+
+          <div className={styles.PurpleCircle}>
+            <h3>Inclusive Living</h3>
+          </div>
+      </div>
+
+        {intlImgs.map((pillars) => {
+            return(
+                <div className={styles.pillarsContainer}>
+                  <div className={styles.card}>
+                    <div className={styles.imgContainer}>
+                      <Image className={styles.cardImgs}
+                      fill
+                      objectFit="cover"
+                      objectPosition="center"
+                      src={pillars.path} 
+                      alt={pillars.altText}
+                     />
+                     </div>
+
+
+
+
+                     <div className={styles.cardText}>
+                      <div className={styles.headerContainer}>
+                           <h2>{pillars.title}</h2>
+
+                            {icons.map((emoji) => (
+                              <Image className={styles.icons}
+                              width={25}
+                              height={25}
+                              src={emoji.path}
+                              />
+                              ))}
+                              </div>
+                             <p className={`body-1`}>{pillars.description}</p>
+
+
+
+                        
+                         
+                        
+        
+
+                  
+                     </div>
+
+                    </div>
+
+
+                </div>
+              
+
+              );
+              
+            })}
+        <div className={styles.learnMore}>
+          <h2 className={styles.learnMoreText}>Want to learn more?</h2>
+          <p className={styles.learnMoreText}>Visit the Best Buddies International Official website.</p>
+        {/*button goes here*/}
+      </div>
+
+    </div>
+
+  );
+}
+
+
+
+{/*<div>
       <div className={styles.heroBox}>
         <img className={styles.heroImg}
         src="/about-imgs/BBI.jpeg"
@@ -36,7 +194,7 @@ export default function AboutIntl() {
           </div>
       </div>
 
-      <div className={styles.cards}> {/*maybe*/}
+      <div className={styles.cards}>
 
 
         <div className={styles.friendships}>
@@ -62,20 +220,20 @@ export default function AboutIntl() {
         </div>
 
         <div className={styles.intergratedEmployment}>
-        {/*img goes here*/}
+        img goes here
           <div className={styles.frame}>
             <h2 className={styles.h2text}>Intergrated Employment</h2> 
         
-            {/*needs emoji*/}
+            needs emoji
               <p>This program secures jobs for people with intellectual and developmental disabilities (IDD), allowing them to earn an income, pay taxes, and continuously and independently support themselves.
               </p>
           </div>
         </div>
 
         <div className={styles.leadershipDevelopment}>
-        {/*img goes here*/}
+        img goes here
           <div className={styles.frame}>
-            <h2 className={styles.h2text}>Leadership Development</h2> {/*needs emoji*/}
+            <h2 className={styles.h2text}>Leadership Development</h2> {/*needs emoji*
               <p>
               These programs educate and empower people with and without IDD to become leaders, public speakers, and advocates. Best Buddies passionately believes that its program participants can lead the effort within their community and beyond to build a more inclusive world for people with IDD.
               </p>
@@ -83,9 +241,9 @@ export default function AboutIntl() {
         </div>
 
         <div className={styles.inclusiveLiving}>
-        {/*img goes here*/}
+        img goes here
           <div className={styles.frame}>
-            <h2>Inclusive Living</h2> {/*needs emoji*/}
+            <h2>Inclusive Living</h2> needs emoji
               <p>
               This program fosters an integrated experience for people with and without IDD to become active, contributing citizens who live independently in a dynamic environment where they can learn, grow, and thrive locations in Washington, D.C, Miami, FL, and Los Angeles, CA.            </p>
           </div>
@@ -95,11 +253,6 @@ export default function AboutIntl() {
       <div className={styles.learnMore}>
         <h2 className={styles.learnMoreText}>Want to learn more?</h2>
         <p className={styles.learnMoreText}>Visit the Best Buddies International Official website.</p>
-        {/*button goes here*/}
+        {/*button goes here
       </div>
-
-    </div>
-
-
-  );
-}
+</div>*/}
--- src/pages/about/ucd/index.jsx
+++ src/pages/about/ucd/index.jsx
@@ -1,71 +1,91 @@
 import styles from "@/styles/pages/about/about-ucd.module.scss";
+import Image from "next/image";
+
+const ucdImgs = [
+  /*{
+    title: "hero",
+    altText: "All the Best Buddies members (about 50 people) are huddled in a group and looking at the camera. In the foreground, four lawn chairs and a table are visible.",
+    path: "/about-imgs/ucd/about-ucd-banner.jpg",
+  },*/
+
+  {
+    title: "Buddies + Peer Buddies",
+    description: "At the beginning of each year, Best Buddies members are matched in one-to-one friendships called 'buddy pairs.' Each buddy pair is made up of one person with IDD (a Buddy) and one person without IDD (a Peer Buddy). Buddy pairs will stay matched for the entire academic year. By signing up to be a Buddy/Peer Buddy, you are committing to coming to chapter events, hanging out with your buddy, and keeping in contact throughout the school year. Members of buddy pairs are required to see their buddy at least once per month and contact them (by phone, text, email, etc.) at least four times per month.",
+    altText: "Two Best Buddies members, both girls, smile at the camera and pose together. In the background is a lush green lawn and trees.",
+    path: "/about-imgs/ucd/about-ucd-buddy.jpg",
+
+  },
+
+  {
+    title: "Associate Members",
+    description: " Another way to get involved is to become an Associate Member. Associates are not matched in a one-to-one friendship. However, they can still attend chapter events and get to know other chapter members. Being an Associate Member is a great way to get involved, especially if you don't think you have enough time to commit to being matched.",
+    altText: "Four Best Buddies members are huddled under an umbrella and smiling at the camera.",
+    path: "/about-imgs/ucd/about-ucd-associate.jpg",
+  },
+
+  {
+    title:"Chapter Events",
+    description: "Each year, we host parties, fundraisers, game nights, and other fun events for our chapter members. Events can include arts and crafts, board games, bowling, lawn games, karaoke, and more. This year's activities will primarily be in person, but we will also hold a few virtual activities for participants who don't feel comfortable with in person events.",
+    altText: "Five Best Buddies members are sitting around a table and smiling at the camera. Multicolored construction paper, scissors, cotton balls, and other craft supplies are on the table.",
+    path: "/about-imgs/ucd/about-ucd-chapter.jpg",
+  },
+
+  {
+    title: "Buddy Pair Hangout",
+    description: "Buddy pairs also hang out outside of our chapter events. What you do with your buddy will depend on what you both are interested in. Our monthly newsletter will include ideas for what buddy pairs can do together. For example, going to the movies, getting boba, and playing basketball together.",
+    altText: "Five Best Buddies members pose together. Three are seated, and two are kneeling behind them. Other members are sitting in the background and chatting.",
+    path: "",
+  }
+];
 
 export default function AboutUCD() {
   return (
-
   <div className={styles.body}>
+      {/* {ucdImgs.map((cardContent) => {
+            return(*/}
 
-  <div className={styles.heroBox}>
-    <img className={styles.heroImg}
-       src="/about-imgs/MatchParty2022.jpg"
-      alt="All the Best Buddies members (about 50 people) are huddled in a group and looking at the camera. In the foreground, four lawn chairs and a table are visible."
-    />
-    
-    <h1 className={styles.heroText}>Best Buddies UC Davis Chapter</h1>
-  </div>
+        <div className={styles.heroContainer}>
+            <Image
+            objectFit="cover" 
+            objectPosition="center"
+            src="/about-imgs/ucd/about-ucd-banner.jpg" 
+            alt="All the Best Buddies members (about 50 people) are huddled in a group and looking at the camera. In the foreground, four lawn chairs and a table are visible."
+            fill
+            />
+            
+            <div className={styles.heroText}>
+              <h1>Best Buddies</h1>
+              <h1>UC Davis Chapter</h1>
+            </div>
+        </div>
 
-  <div className={styles.cards}>
-    <div className={styles.cardsBox}>
-      <img className={styles.imgs} 
-      width={548}
-      height={637}
-      src="/about-imgs/Adairandkarissa.jpg" 
-      alt="Two Best Buddies members, both girls, smile at the camera and pose together. In the background is a lush green lawn and trees." 
-      />
-
-      <div className={styles.buddies}>
-        <h2>Buddies + Peer Buddies</h2>
-        <p>
-          At the beginning of each year, Best Buddies members are matched in one-to-one friendships called "buddy pairs." Each buddy pair is made up of one person with IDD (a Buddy) and one person without IDD (a Peer Buddy). Buddy pairs will stay matched for the entire academic year. By signing up to be a Buddy/Peer Buddy, you are committing to coming to chapter events, hanging out with your buddy, and keeping in contact throughout the school year. Members of buddy pairs are required to see their buddy at least once per month and contact them (by phone, text, email, etc.) at least four times per month.
-        </p>
-      </div>
-    </div>
-
-    <div className={styles.cardsBox}>
-      <div className={styles.associateMembers}>
-        <h2>Associate Members</h2>
-        <p>
-          Another way to get involved is to become an Associate Member. Associates are not matched in a one-to-one friendship. However, they can still attend chapter events and get to know other chapter members. Being an Associate Member is a great way to get involved, especially if you don't think you have enough time to commit to being matched.
-        </p>
-      </div>
-
-        {/*this img needs to be fixed*/}
-      <img
-         width={548}
-        height={637}
-        src="/about-imgs/Caroline, Tyler, Matt, Sean.jpg"
-        alt="Four Best Buddies members are huddled under an umbrella and smiling at the camera."
-      />
+      <div className={styles.cardsContainer}>
+        {ucdImgs.map((cardContent) => {
+            return(
+              <div className={styles.card}>
+                
+                <div className={styles.imgContainer}>
+                  <Image className={styles.cardImgs}
+                  fill
+                  objectFit="cover"
+                  objectPosition="center"
+                  src={cardContent.path} 
+                  alt={cardContent.altText}
+                  />
+                </div>
+                
+                <div className={styles.cardText}>
+                  <h2>{cardContent.title}</h2>
+                  <p className={`body-1`}>{cardContent.description}</p>
+                </div>
+              </div>
 
 
-    </div>
+                );
+            })}
+          </div>
 
-      <div className={styles.chapterEvents}>
-      {/* img here */}
-        <h2>Chapter Events</h2>
-        <p>
-        Each year, we host parties, fundraisers, game nights, and other fun events for our chapter members. Events can include arts and crafts, board games, bowling, lawn games, karaoke, and more. This year's activities will primarily be in person, but we will also hold a few virtual activities for participants who don't feel comfortable with in person events.
-        </p>
-      </div>
 
-      <div className={styles.buddyPair}>
-      {/* img here */}
-        <h2>Buddy Pair Hangouts</h2>
-        <p>
-        Buddy pairs also hang out outside of our chapter events. What you do with your buddy will depend on what you both are interested in. Our monthly newsletter will include ideas for what buddy pairs can do together. For example, going to the movies, getting boba, and playing basketball together.
-        </p>
-      </div>
-    </div>
 
   </div>
   );
