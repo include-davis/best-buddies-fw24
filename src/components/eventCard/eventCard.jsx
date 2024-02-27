58c6f503ac024715f6cacb0b351d627376d99342
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-02-27 01:01:02 -0800

    added prop for alt text

--- src/components/eventCard/eventCard.jsx
+++ src/components/eventCard/eventCard.jsx
@@ -2,11 +2,11 @@ import styles from "@/styles/components/eventcard/eventcard.module.scss";
 import Image from "next/image";
 import Button from "../button/button";
 
-export default function EventCard({ title, date, description, imagePath, eventLink }) {
+export default function EventCard({ title, date, description, imagePath, altText, eventLink }) {
     return (
       <div className={styles.card}>
         <div className={styles.image}>
-          <Image src={imagePath} alt={title} width={352} height={240} />
+          <Image src={imagePath} alt={altText} width={352} height={240} />
         </div>
         <h3 className={styles.cardTitle}>{title}</h3>
         <p className={styles.cardSubtitle}>{date}</p>
