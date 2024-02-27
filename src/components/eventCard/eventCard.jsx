8d31f9d9e4ca482927aa46f45c5ed2c67d14f2f0
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-02-27 01:22:24 -0800

    fixed flex box and spacing

--- src/components/eventCard/eventCard.jsx
+++ src/components/eventCard/eventCard.jsx
@@ -4,13 +4,17 @@ import Button from "../button/button";
 
 export default function EventCard({ title, date, description, imagePath, altText, eventLink }) {
     return (
-      <div className={styles.card}>
-        <div className={styles.image}>
-          <Image src={imagePath} alt={altText} width={352} height={240} />
+      <div className={styles.card}> {/* 40 px gaps between info, button */}
+        <div className={styles.eventInfo}> {/* 16 px gaps between image, title+date, description */}
+          <div className={styles.image}>
+            <Image src={imagePath} alt={altText} width={352} height={240} />
+          </div>
+          <div className={styles.titleDateContainer}>
+            <h3 className={styles.cardTitle}>{title}</h3>
+            <p className={styles.cardSubtitle}>{date}</p>
+          </div>
+          <p className={styles.cardDescription}>{description}</p>
         </div>
-        <h3 className={styles.cardTitle}>{title}</h3>
-        <p className={styles.cardSubtitle}>{date}</p>
-        <p className={styles.cardDescription}>{description}</p>
         <Button label="RSVP" href={eventLink} />
       </div>
     );
