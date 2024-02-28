74ccd30b45c76ce8a8d17a163013a95e85f01c60
anna-c2 <annacai246@gmail.com>
2024-02-27 18:58:04 -0800

    updated footer links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -22,48 +22,32 @@ const  smicons = [
 
 ];
 
-/*const  phoneAndEmailIcons = [
-  {
-    name: "phone",
-    path: "/footer-icons/phone.svg",
-  },
-  {
-    name: "email",
-    path: "/footer-icons/mail.svg",
-    slug: "mailto:",
-  },
-];*/
-
 const sections = [
   {
     title: 'About Us',
-    titleURL: 'https://www.facebook.com/bestbuddiesatucdavis',
     links: [
-      { label: 'UCD Chapter', url: '' },
-      { label: 'Best Buddies International', url: '' }
+      { label: 'UCD Chapter', url: '/about/ucd' },
+      { label: 'Best Buddies International', url: '/about/intl' }
     ]
   },
   {
     title: 'Events',
-    titleURL: 'https://www.facebook.com/bestbuddiesatucdavis',
     links: [
-      { label: 'Chapter Events', url: '' },
-      { label: 'Regional and State Events', url: '' }
+      { label: 'Chapter Events', url: '/event/chapter' },
+      { label: 'Regional and State Events', url: '/event/regional-state' }
     ]
   },
   {
     title: 'Members',
-    titleURL: 'https://www.facebook.com/bestbuddiesatucdavis',
     links: [
-      { label: 'New Members', url: '' },
-      { label: 'Current Members', url: '' }
+      { label: 'New Members', url: '/member/new' },
+      { label: 'Current Members', url: '/member/current' }
     ]
   },
   {
     title: 'Contact Us',
-    titleURL: 'https://www.facebook.com/bestbuddiesatucdavis',
     links: [
-      { label: 'Contact Form', url: '' }
+      { label: 'Contact Form', url: '/contact' }
     ]
   }
 ];
@@ -73,42 +57,39 @@ export default function Footer() {
     <footer>
       <div className={styles.container}>
         <div className={styles.logoAndSocials}>
-        <div className={styles.socials}>
-          <Image width={124} height={124} src="/best-buddies-logo.svg" alt="best-buddies-logo"/>
-          <div className={styles.socialsRight}>
-            
-              <p className={`body-1-bold`}>BEST BUDDIES</p>
-              <p className={`body-1-bold`}>AT UC DAVIS</p>
-            
-            <div className={styles.smicons}>
-              {smicons.map((icon) => {
-                return <Link href={icon.slug} key={icon.name} target="_blank">
-                  <Image width={25} height={25} src={icon.path} alt={icon.name} />
-                </Link>
-              })}
-            </div>
-
-          </div>
-        </div>
-        <div className={styles.infoSection}>
-            <div className={styles.email}>
-              <Image width={25} height={25} className={styles.logo} src={"/footer-icons/mail.svg"} alt={"email"}/>
-              <p className={`body-1`}>ucdbestbuddies@gmail.com</p>
-            </div>
-            <div className={styles.phone}>
-              <Image width={25} height={25} src={"/footer-icons/phone.svg"} alt={"phone"}/>
-              <p className={`body-1`}>916 - 538 - 3662</p>
+          <div className={styles.socials}>
+            <Image width={124} height={124} src="/best-buddies-logo.svg" alt="best-buddies-logo"/>
+            <div className={styles.socialsRight}>
+                <p className={`body-1-bold`}>BEST BUDDIES</p>
+                <p className={`body-1-bold`}>AT UC DAVIS</p> 
+              <div className={styles.smicons}>
+                {smicons.map((icon) => {
+                  return <Link href={icon.slug} key={icon.name} target="_blank">
+                    <Image width={25} height={25} src={icon.path} alt={icon.name} />
+                  </Link>
+                })}
+              </div>
             </div>
           </div>
+          <div className={styles.infoSection}>
+              <div className={styles.email}>
+                <Image width={25} height={25} className={styles.logo} src={"/footer-icons/mail.svg"} alt={"email"}/>
+                <p className={`body-1`}>ucdbestbuddies@gmail.com</p>
+              </div>
+              <div className={styles.phone}>
+                <Image width={25} height={25} src={"/footer-icons/phone.svg"} alt={"phone"}/>
+                <p className={`body-1`}>916 - 538 - 3662</p>
+              </div>
+            </div>
         </div>
         <div className={`${styles.sections} body-1`}>
           <Link href="www.google.com" className={`${styles.home} body-1-bold`}>Home</Link>
           {sections.map((section, index) => (
             <div key={index} className={styles.section}>
-            <Link href={section.titleURL} className={`body-1-bold`}>{section.title}</Link>
-            {section.links.map((link, linkIndex) => (
-              <Link key={linkIndex} href={link.url} target="_blank">{link.label}</Link>
-            ))}
+              <p className={`body-1-bold`}>{section.title}</p>
+              {section.links.map((link, linkIndex) => (
+                <Link key={linkIndex} href={link.url} target="_blank" className={link.label === 'Contact Form' ? `${styles.contactFormLink}` : ''}>{link.label}</Link>
+              ))}
             </div>
           ))}
         </div>
@@ -117,7 +98,6 @@ export default function Footer() {
         <p className={styles.bestBuddies}>Best Buddies at UC Davis</p>
         <p>Made with 🤍 by #include at Davis © 2024</p>
       </div>
-      
     </footer>
   );
 }
