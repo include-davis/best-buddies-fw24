6801d22ec3ce3aaecfb69ee27bebf1513b0f84c2
anna-c2 <annacai246@gmail.com>
2024-02-29 09:33:33 -0800

    mapped contact dropdown options to replace repetitive code

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -7,13 +7,13 @@ import Image from "next/image";
 
 export default function Contact() {
 
-  /*const options = [
-    { id: 'bestbuddiesinfo', label: 'What Best Buddies does and how it works at UC Davis' },
-    { id: 'join', label: 'How to join Best Buddies' },
-    { id: 'events', label: 'Best Buddies events' },
-    { id: 'conflict', label: 'Buddy/peer buddy conflict' },
-    { id: 'other', label: 'Other' }
-  ];*/
+  const options = [
+    { id: 'bestbuddiesinfo', content: 'What Best Buddies does and how it works at UC Davis' },
+    { id: 'join', content: 'How to join Best Buddies' },
+    { id: 'events', content: 'Best Buddies events' },
+    { id: 'conflict', content: 'Buddy/peer buddy conflict' },
+    { id: 'other', content: 'Other' }
+  ];
   const [selected, setSelected] = useState('');
   const [optionsActive, setOptionsActive] = useState(false);
 
@@ -51,31 +51,12 @@ export default function Contact() {
             <p className={`${styles.required} body-1-bold`}>I have a question/comment about ...</p>
             <div className={styles.selectBox}>
               <div className={optionsActive ? `${styles.optionsContainer} ${styles.active}` : `${styles.optionsContainer}`}>
-                {/* use content instead of label */}
-                <div className={styles.option} onClick={selectOption}>
-                  <input type="radio" className={styles.radio} id="bestbuddiesinfo" name="category"></input>
-                  <label for="bestbuddiesinfo">What Best Buddies does and how it works at UC Davis</label>
-                </div>
-
-                <div className={styles.option} onClick={selectOption}>
-                  <input type="radio" className={styles.radio} id="join" name="category"></input>
-                  <label for="join">How to join Best Buddies</label>
-                </div>
-
-                <div className={styles.option} onClick={selectOption}>
-                  <input type="radio" className={styles.radio} id="events" name="category"></input>
-                  <label for="events">Best Buddies events</label>
-                </div>
-
-                <div className={styles.option} onClick={selectOption}>
-                  <input type="radio" className={styles.radio} id="conflict" name="category"></input>
-                  <label for="conflict">Buddy/peer buddy conflict</label>
-                </div>
-
-                <div className={styles.option} onClick={selectOption}>
-                  <input type="radio" className={styles.radio} id="other" name="category"></input>
-                  <label for="other">Other</label>
-                </div>
+                  {options.map((option, index) => (
+                    <div key={index} className={styles.option} onClick={selectOption}>
+                      <input type="radio" className={styles.radio} id={option.id} name="category"></input>
+                      <label for={option.id}>{option.content}</label>
+                    </div>
+                  ))}
               </div>
               <div onClick={toggleOptionsMenu} className={styles.selected}>
                 <div className={optionsActive ? `${styles.selectedChoice}` : `${styles.hide}`}>
@@ -98,3 +79,28 @@ export default function Contact() {
   </div>;
 }
 
+//non-mapping code in case i need it
+/* <div className={styles.option} onClick={selectOption}>
+                  <input type="radio" className={styles.radio} id="bestbuddiesinfo" name="category"></input>
+                  <label for="bestbuddiesinfo">What Best Buddies does and how it works at UC Davis</label>
+                </div>
+
+                <div className={styles.option} onClick={selectOption}>
+                  <input type="radio" className={styles.radio} id="join" name="category"></input>
+                  <label for="join">How to join Best Buddies</label>
+                </div>
+
+                <div className={styles.option} onClick={selectOption}>
+                  <input type="radio" className={styles.radio} id="events" name="category"></input>
+                  <label for="events">Best Buddies events</label>
+                </div>
+
+                <div className={styles.option} onClick={selectOption}>
+                  <input type="radio" className={styles.radio} id="conflict" name="category"></input>
+                  <label for="conflict">Buddy/peer buddy conflict</label>
+                </div>
+
+                <div className={styles.option} onClick={selectOption}>
+                  <input type="radio" className={styles.radio} id="other" name="category"></input>
+                  <label for="other">Other</label>
+                </div> */
