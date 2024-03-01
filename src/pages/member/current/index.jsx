654f2025aa66e035d3f154c8103a0ef40b7f5ddd
Vivica Tran <viamtran@ucsc.edu>
2024-02-29 23:05:11 -0800

    added photo

--- src/pages/member/current/index.jsx
+++ src/pages/member/current/index.jsx
@@ -1,4 +1,6 @@
 import styles from "@/styles/pages/member/current.module.scss";
+import Image from "next/image";
+
 
 export default function CurrentMembers() {
   return (
@@ -8,29 +10,39 @@ export default function CurrentMembers() {
         <p className={styles.renewalTitle}>Renewing your Membership?</p>
         <p className={styles.renewalBoxContent}>
           Keep your Best Buddies membership active by submitting a new application. Renewals are required annually, even for long-time club members. 
-          <br></br>
-          <br></br>
+          <br/>
+          <br/>
           Check our YouTube channel for application guidance. Interviews may be necessary, but officers will inform you in May or June if required this year.
         </p>
         <button className={styles.renewalButton}>Renewal Application
           <img src="/page-icons/hyperlink.svg" alt="Renewal Application"/>
         </button>
       </div>
+      <div className={styles.videoBox}>yt video here</div>
       <div className={styles.friendUpdatesBox}>
         <p className={styles.friendUpdatesTitle}>Friend Updates!</p>
         <p className={styles.friendUpdatesContent}>
           Starting in November, every Buddy and Peer Buddy will need to complete a Friendship Update each month. These updates let us know how your friendship is going and if you need support.
         </p>
-        <button className={styles.friendUpdatesButton}>Friendship Updates</button>
+        <button className={styles.friendUpdatesButton}>Friendship Updates
+        <img src="/page-icons/hyperlink.svg" alt="Friendship Updates"/>
+        </button>
       </div>
       <div className={styles.latestNewsBox}>
         <p className={styles.latestNewsTitle}>Our Latest News</p>
         <p className={styles.latestNewsContent}>
           We will send out newsletters every month from September to June. There you can find announcements, upcoming chapter events, community events, and ideas for things to do with your buddy.
         </p>
-        <button className={styles.latestNewsButton}>Dec 2023
-          <img src="/page-icons/hyperlink.svg" alt="Latest News"/>
-        </button>
+        <div className={styles.prevNewsBox}>
+          <button className={styles.latestNewsButton}>Dec 2023
+            <img src="/page-icons/hyperlink.svg" alt="Latest News"/>
+          </button>
+          <Image className={styles.newsletterImg}
+            width = {696}
+            height = {483}
+            src="/member-imgs/best_buddies_newsletter_img.png"
+          />
+        </div>
       </div>
     </div>
   );
