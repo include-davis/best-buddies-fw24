a653d41dbaac6e2701962ecd2a873cf07fb18874
Ryan <ryyu444@gmail.com>
2024-02-29 22:44:57 -0800

    reformatted

--- src/components/eventCard/eventCard.jsx
+++ src/components/eventCard/eventCard.jsx
@@ -2,19 +2,29 @@ import styles from "@/styles/components/eventcard/eventcard.module.scss";
 import Image from "next/image";
 import Button from "../button/button";
 
-export default function EventCard({ title, date, description, imagePath, altText, eventLink }) {
-    return (
-      <div className={styles.card}> {/* 40 px gaps between info, button */}
-       <div className={styles.eventInfo}> {/* 16 px gaps between image, title+date, description */}
-          <Image src={imagePath} alt={altText} width={352} height={240} />
-          <div className={styles.titleDateContainer}> 
-            <h3 className={styles.cardTitle}>{title}</h3>
-            <p className={styles.cardSubtitle}>{date}</p>
-            <p className={styles.cardDescription}>{description}</p>
-          </div>
+export default function EventCard({
+  title,
+  date,
+  description,
+  imagePath,
+  altText,
+  eventLink,
+}) {
+  return (
+    <div className={styles.card}>
+      {" "}
+      {/* 40 px gaps between info, button */}
+      <div className={styles.eventInfo}>
+        {" "}
+        {/* 16 px gaps between image, title+date, description */}
+        <Image src={imagePath} alt={altText} width={352} height={240} />
+        <div className={styles.titleDateContainer}>
+          <h3 className={styles.cardTitle}>{title}</h3>
+          <p className={styles.cardSubtitle}>{date}</p>
+          <p className={styles.cardDescription}>{description}</p>
         </div>
-        <Button label="RSVP" href={eventLink} />
       </div>
-    );
-  }
-  
+      <Button label="RSVP" href={eventLink} />
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,25 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Button from "@/components/button/button";
+import EventCard from "@/components/eventCard/eventCard";
+
+const events = [
+  {
+    title: "Event 1",
+    date: "2022-01-01",
+    description: "Event 1 description",
+    imagePath: "/event1.jpg",
+    altText: "Event 1 image",
+    eventLink: "/event1",
+  },
+];
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className={`body-1`}>
+      <EventCard />
     </div>
   );
 }
