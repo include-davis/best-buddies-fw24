a26c96fcedc6d0a4d09bf16fa51e407a5385cb30
Vivica Tran <viamtran@ucsc.edu>
2024-03-03 19:21:35 -0800

    working on mobile view, added yt vid and newsletter links

--- src/pages/member/current/index.jsx
+++ src/pages/member/current/index.jsx
@@ -19,8 +19,64 @@ export default function CurrentMembers() {
           <img className={styles.renewalButton} src="/page-icons/hyperlink.svg"/>
         </button>
       </div>
+      {/*using from new-members page*/}
+      <div className={styles.youtubeContainer}>
+        <div className={styles.youtubeCard}>
+          <iframe 
+            className={styles.vid}
+            width="560" height="315"
+            src="https://www.youtube-nocookie.com/embed/jpDCf0O0rsc?si=qUAJXnxWaqNqUjpF&amp;controls=0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen>
+          </iframe>
+          <div className={styles.footerContainer}>
+          <div className={styles.providerContainer}>
+            <h4>
+              How to submit a membership application
+              <br />
+              Buddies...
+            </h4>
+            <div className={styles.providerItems}>
+              {/*need new logoe*/}
+              <img src="/footer-icons/youtube.svg"/>
+              <p>YouTube</p>
+            </div>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="17"
+            viewBox="0 0 16 17"
+            fill="none"
+          >
+            <g clipPath="url(#clip0_2591_2184)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1 8.5C1 4.634 4.134 1.5 8 1.5C11.866 1.5 15 4.634 15 8.5C15 12.366 11.866 15.5 8 15.5C4.134 15.5 1 12.366 1 8.5ZM8 16.5C3.582 16.5 0 12.918 0 8.5C0 4.082 3.582 0.5 8 0.5C12.418 0.5 16 4.082 16 8.5C16 12.918 12.418 16.5 8 16.5ZM7.25 12.75V7.75H8.75V12.75H7.25ZM9 5.5C9 6.052 8.552 6.5 8 6.5C7.448 6.5 7 6.052 7 5.5V5.431C7 4.879 7.448 4.431 8 4.431C8.552 4.431 9 4.879 9 5.431V5.5Z"
+                fill="black"
+                fillOpacity="0.8"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2591_2184">
+                <rect
+                  width="16"
+                  height="16"
+                  fill="white"
+                  transform="translate(0 0.5)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+      </div>
+      </div>
+      {/*using from new-members page*/}
 
-      <div className={styles.videoBox}>yt video here</div>
+
 
       <div className={styles.friendUpdatesBox}>
         <p className={styles.subPageTitle}>Friendship Updates!</p>
@@ -42,13 +98,30 @@ export default function CurrentMembers() {
         <div className={styles.prevNewsBox}>
           <button className={styles.latestNewsButton}>Dec 2023
             <img src="/page-icons/hyperlink.svg"/>
+
           </button>
-          <Image className={styles.newsletterImg}
-            width = {696}
-            height = {483}
-            src="/member-imgs/best_buddies_newsletter_img.png"
-          />
+          <div className={styles.prevNewsImgContainer}>
+            <Image
+                  className={styles.newsletterImg}
+                  width={696}
+                  height={483}
+                  src="/member-imgs/best_buddies_newsletter_img.png"
+              />
+            <div className={styles.pastNewsColBox}>
+              <h2>Past Newsletters</h2>
+              <div className={styles.prevNewsLinksContainer}>
+              <button className={styles.prevNewsLinks}>Oct 2023
+                <img src="/page-icons/hyperlink.svg"/>
+              </button>
+              <button className={styles.prevNewsLinks}>Nov 2023
+                <img src="/page-icons/hyperlink.svg"/>
+              </button>
+              </div>
+            </div>
+          </div>
         </div>
+
+
 
       </div>
 
