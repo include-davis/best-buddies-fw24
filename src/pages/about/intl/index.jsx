76a3eac09ecfdc90a96119cf34a004b5e97405c1
Ryan <ryyu444@gmail.com>
2024-03-04 02:19:40 -0800

    Reformat + Added circle component

--- src/pages/about/intl/index.jsx
+++ src/pages/about/intl/index.jsx
@@ -1,6 +1,8 @@
 import styles from "@/styles/pages/about/about-intl.module.scss";
 import Image from "next/image";
+import FourPillars from "@/components/about/fourPillars/FourPillars";
 import Button from "@/components/button/button";
+import AutoImage from "@/components/AutoImage/AutoImage";
 
 const intlContent = [
   {
@@ -49,10 +51,9 @@ export default function AboutIntl() {
     <div className={styles.body}>
       <div className={styles.heroContainer}>
         <Image
+          className={styles.heroImg}
           src="/about-imgs/intl/about-intl-banner.jpg"
           alt="Large group of members posing together in front of a banner and a purple and white ballon arch."
-          objectFit="cover"
-          objectPosition="center"
           fill
         />
         <div className={styles.heroText}>
@@ -62,63 +63,42 @@ export default function AboutIntl() {
       </div>
 
       <div className={styles.pillars}>
-        <h2 className={styles.pillarsHeader}>Our Four Pillars</h2>
-        <div className={styles.circles}>
-          <div className={styles.topCircles}>
-            <div className={styles.YellowCircle}>
-              <h3>One-to-One Friendships</h3>
-            </div>
-
-            <div className={styles.GreenCircle}>
-              <h3>Intergrated Employment</h3>
-            </div>
-          </div>
-
-          <div className={styles.bottomCircles}>
-            <div className={styles.RedCircle}>
-              <h3>Leadership Development</h3>
-            </div>
-
-            <div className={styles.PurpleCircle}>
-              <h3>Inclusive Living</h3>
-            </div>
-          </div>
+        <div className={styles.pillarsContainer}>
+          <FourPillars />
         </div>
 
-        <div className={styles.pillarsContainer}>
+        <div className={styles.pillarsCards}>
           {intlContent.map((pillars, index) => {
             return (
-                <div key={index}
+              <div
+                key={index}
                 className={`${styles.card} ${
                   index % 2 == 0 ? styles.imgLeft : styles.imgRight
                 }`}
               >
-                  <div className={styles.imageContainer}>
+                <div className={styles.imageContainer}>
+                  <AutoImage
+                    className={styles.cardImg}
+                    src={pillars.imagePath}
+                    alt={pillars.altText}
+                  />
+                </div>
+
+                <div className={styles.cardText}>
+                  <div className={styles.headerContainer}>
+                    <h2>{pillars.title}</h2>
+
                     <Image
-                      className={styles.cardImgs}
-                      fill
-                      objectFit="cover"
-                      objectPosition="center"
-                      src={pillars.imagePath}
-                      alt={pillars.altText}
+                      className={styles.icons}
+                      width={44}
+                      height={44}
+                      src={pillars.iconPath}
                     />
                   </div>
 
-                  <div className={styles.cardText}>
-                    <div className={styles.headerContainer}>
-                      <h2>{pillars.title}</h2>
-
-                      <Image
-                        className={styles.icons}
-                        width={44}
-                        height={44}
-                        src={pillars.iconPath}
-                      />
-                    </div>
-
-                    <p className={`body-1`}>{pillars.description}</p>
-                  </div>
+                  <p className={`body-1`}>{pillars.description}</p>
                 </div>
+              </div>
             );
           })}
         </div>
