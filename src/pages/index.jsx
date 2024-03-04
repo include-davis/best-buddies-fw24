0b810dd5705357fed8fc332013f7d148445c6134
Ryan <ryyu444@gmail.com>
2024-03-04 00:36:08 -0800

    Made event card fully responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/pages/home/home.module.scss";
-import AutoImage from "@/components/AutoImage/AutoImage";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
