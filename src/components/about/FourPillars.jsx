8410c3cd3c3d722443b8bea64059d58a574ec95d
Austin Shih <austinhshih@gmail.com>
2024-03-03 23:01:20 -0800

    Finished circles :D

---
+++ src/components/about/FourPillars.jsx
@@ -0,0 +1,28 @@
+import styles from "@/styles/components/about/FourPillars.module.scss";
+
+export default function FourPillars() {
+  return (
+    <div className={styles.container}>
+        <div>
+            <div className={styles.circle1}>
+                <p>One-To-One</p>
+                <p>Friendships</p>
+            </div>
+            <div className={styles.circle2}>
+                <p>Integrated</p>
+                <p>Employment</p>
+            </div> 
+        </div>
+        <div className={styles.container2}>
+            <div className={styles.circle3}>
+                <p>Leadership</p>
+                <p>Development</p>
+            </div>
+            <div className={styles.circle4}>
+                <p>Inclusive</p>
+                <p>Living</p>
+            </div>
+        </div>
+    </div>
+  );
+}
