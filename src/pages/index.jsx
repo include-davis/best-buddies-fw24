58844850b1825db29b02e6d2c3cf2ad6e80231c9
Ryan <ryyu444@gmail.com>
2024-03-03 23:30:04 -0800

    modified font colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/pages/home/home.module.scss";
+import AutoImage from "@/components/AutoImage/AutoImage";
 
 const inter = Inter({ subsets: ["latin"] });
 
