0419e63f4af498ceb4a92ad0d3768b712b48a28b
anna-c2 <annacai246@gmail.com>
2024-03-04 00:58:20 -0800

    added comments and updated mobile nav bar
    hide mobile nav bar links everytime user loads a new page

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 import Dropdown from "@/components/navbar/dropdown/dropdown";
 import Button from "@/components/button/button";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 
 const navLinks = [
   {name: "Home", href: "/home", menu: false},
@@ -23,10 +25,15 @@ const navLinks = [
 ]
 
 export default function Navbar() {
-  
+  //show or hide dropdown menu
   const [dropdownStates, setDropdownStates] = useState(navLinks.map(() => false));
+  //show or hide mobile navbar menu with the links
   const [mobileMenuState, setMobileMenuState] = useState(false);
+  //bars icon to open mobile navbar menu or x icon to close mobile navbar menu
   const [mobileMenuIcon, setMobileMenuIcon] = useState("page-icons/bars.svg");
+  const router = useRouter();
+
+  //show or hide the dropdown menus
   const toggleMenu = (index) => {
     setDropdownStates((prevStates) => {
       const newStates = prevStates.map((state, i) => i == index ? !state : false);
@@ -34,6 +41,7 @@ export default function Navbar() {
     });
   };
 
+  //show or hide mobile navbar 
   const toggleMobileMenu = () => {
     if(mobileMenuState){
       setMobileMenuState(false);
@@ -45,6 +53,12 @@ export default function Navbar() {
     }
   };
 
+  //NOT SURE IF THIS FEATURE IS NEEDED: close mobile navbar menu everytime user loads to a new page (couldn't use onClick for Button component)
+  useEffect(() => {
+    setMobileMenuState(false);
+  }, [router.asPath]);
+
+  //show dropdown menu under category
   const renderDropdownMenu = (links, isActive, index) => {
     return(
       <Dropdown state={isActive} setState={(state) => setDropdownStates((prevStates) =>
@@ -52,6 +66,7 @@ export default function Navbar() {
         newStates[index] = state;
         return newStates;
         })}>
+        {/* show links under the corresponding category */}
         {links.map((dropdownLink, linkIndex) => (
           <Link key={linkIndex} className={styles.link} href={dropdownLink.url}>{dropdownLink.label}</Link>
         )
@@ -66,16 +81,20 @@ export default function Navbar() {
         <div className={styles.logo}>
           <Image width={86} height={86} alt="logo" src="/best-buddies-logo.svg" />
         </div>
+        {/* categories, join button, bars/x icon on mobile */}
         <div className={styles.rightSide}>
           <ul className={styles.links}>
+            {/* categories */}
             {navLinks.map((link, index) => (
               <li className={styles.pageLink} key={link.name}>
+                {/* if the category has a dropdown menu, render it when the user's mouse enters the category*/}
                 {link.menu ? (
                   <div className={`${styles.header} body-1`} onMouseEnter={() => toggleMenu(index)}>
                     <div className={styles.headerContent}>
                       <p>{link.name}</p>
                       <Image className={styles.dropdownIcon} width={24} height={27} src="page-icons/dropdown.svg"/>
                     </div>
+                
                     {dropdownStates[index] && renderDropdownMenu(link.links, dropdownStates[index], index)}
                   </div>) : (
                     <Link className={`body-1`} href={`${link.href}`}>
@@ -106,13 +125,16 @@ export default function Navbar() {
                 {dropdownStates[index] && (
                   <Dropdown key={index} state={dropdownStates[index]} isMobile={true}>
                     {navLinks[index].links.map((dropdownLink, index) => (
+                      
                       <Button key={index} className={styles.link} href={dropdownLink.url} label={dropdownLink.label}/>
+                     
                     ))}
                   </Dropdown>
                 )}
                 </div>
               ) : (
-                <Button key={index} href="" label={link.name}></Button>
+                
+                  <Button key={index} href="" label={link.name}></Button>
               )}
             </div>
             ))}
