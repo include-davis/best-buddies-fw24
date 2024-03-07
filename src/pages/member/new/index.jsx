95f7be5638ff6bcbe50cffd4152f1a84c3aa489b
David Lee <72262475+dql1116@users.noreply.github.com>
2024-03-06 16:22:09 -0800

    worked on youtube portion of page

--- src/pages/member/new/index.jsx
+++ src/pages/member/new/index.jsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/pages/member/new.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import YouTubePlayer from '@/components/YouTubePlayer/YouTubePlayer.jsx';
 
 
 const howToCardData = [
@@ -52,7 +53,7 @@ export default function NewMembers() {
           />
         </div>
         <div className={styles.joinInfo}>
-          <h2 className={styles.joinTitle}>Who Can Join?</h2>
+          <h2>Who Can Join?</h2>
           <div className={styles.joinDesc}>
             <p className={"body-1"}>
               Anyone can join Best Buddies, regardless of whether you have an
@@ -125,125 +126,23 @@ export default function NewMembers() {
 
       {/* Need Help */}
       <div className={styles.helpContainer}>
-        <div className={styles.headerContainer}>
+        <div className={styles.helpHeaderContainer}>
           <h2>Need Help?</h2>
-          <p>Check out our video resources for help with your application.</p>
+          <p className={`body-1 ${styles.helpHeaderText}`}>Check out our video resources for help with your application.</p>
         </div>
+        
 
-        <div className={styles.textBox}>
-          <h3>Buddies & Support Staff</h3>
-          <h3>Peer Buddies</h3>
-        </div>
-
-        {/* card */}
-        {/* <div className={styles.youtubeContainer}>
-          <div className={styles.youtubeCard}>
-            <iframe
-              className={styles.video}
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/jpDCf0O0rsc?si=QcoocIa8h9u8W-mN"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-            <div className={styles.footerContainer}>
-              <div className={styles.providerContainer}>
-                <h4>
-                  How to submit a membership application
-                  <br></br>
-                  Buddies...
-                </h4>
-                <div className={styles.providerItems}>
-                  <p>logo</p>
-                  <p>YouTube</p>
-                </div>
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="17"
-                viewBox="0 0 16 17"
-                fill="none"
-              >
-                <g clip-path="url(#clip0_2591_2184)">
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M1 8.5C1 4.634 4.134 1.5 8 1.5C11.866 1.5 15 4.634 15 8.5C15 12.366 11.866 15.5 8 15.5C4.134 15.5 1 12.366 1 8.5ZM8 16.5C3.582 16.5 0 12.918 0 8.5C0 4.082 3.582 0.5 8 0.5C12.418 0.5 16 4.082 16 8.5C16 12.918 12.418 16.5 8 16.5ZM7.25 12.75V7.75H8.75V12.75H7.25ZM9 5.5C9 6.052 8.552 6.5 8 6.5C7.448 6.5 7 6.052 7 5.5V5.431C7 4.879 7.448 4.431 8 4.431C8.552 4.431 9 4.879 9 5.431V5.5Z"
-                    fill="black"
-                    fill-opacity="0.8"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_2591_2184">
-                    <rect
-                      width="16"
-                      height="16"
-                      fill="white"
-                      transform="translate(0 0.5)"
-                    />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
+        <div className={styles.helpContents}>
+          <div className={styles.youtubeTitle}>
+            <h3>Buddies & Support Staff</h3>
+            <h3>Peer Buddies</h3>
           </div>
 
-          <div className={styles.youtubeCard}>
-            <iframe
-              className={styles.video}
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/RN8OdsNe_50?si=S4fMfa6gA61n4E6i"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-            <div className={styles.footerContainer}>
-              <div className={styles.providerContainer}>
-                <h4>
-                  How to submit a membership application on BB360
-                  <br></br>
-                  (...
-                </h4>
-                <div className={styles.providerItems}>
-                  <p>logo</p>
-                  <p>YouTube</p>
-                </div>
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="17"
-                viewBox="0 0 16 17"
-                fill="none"
-              >
-                <g clip-path="url(#clip0_2591_2184)">
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M1 8.5C1 4.634 4.134 1.5 8 1.5C11.866 1.5 15 4.634 15 8.5C15 12.366 11.866 15.5 8 15.5C4.134 15.5 1 12.366 1 8.5ZM8 16.5C3.582 16.5 0 12.918 0 8.5C0 4.082 3.582 0.5 8 0.5C12.418 0.5 16 4.082 16 8.5C16 12.918 12.418 16.5 8 16.5ZM7.25 12.75V7.75H8.75V12.75H7.25ZM9 5.5C9 6.052 8.552 6.5 8 6.5C7.448 6.5 7 6.052 7 5.5V5.431C7 4.879 7.448 4.431 8 4.431C8.552 4.431 9 4.879 9 5.431V5.5Z"
-                    fill="black"
-                    fill-opacity="0.8"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_2591_2184">
-                    <rect
-                      width="16"
-                      height="16"
-                      fill="white"
-                      transform="translate(0 0.5)"
-                    />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
+          <div className={styles.youtubeContainer}>
+            <YouTubePlayer className={styles.youtubePlayer} src="https://www.youtube.com/embed/jpDCf0O0rsc?si=7ekvB55Uo2b3kAGN" title="How to submit a membership application (BUDDIES + SUPPORT STAFF)"/>
+            <YouTubePlayer className={styles.youtubePlayer} src="https://www.youtube.com/embed/RN8OdsNe_50?si=CtJZe17_IcmjHgkL" title="How to submit a membership application on BB360 (FOR PEER BUDDIES)"/>
           </div>
-        </div> */}
-        {/* card */}
+        </div>
       </div>
     </div>
   );
