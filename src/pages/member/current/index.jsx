30240c6d0d342381cf82b36b603d525752b46910
ylylime <leyi1233@gmail.com>
2024-03-08 01:45:23 -0800

    newsletter uploaded

--- src/pages/member/current/index.jsx
+++ src/pages/member/current/index.jsx
@@ -16,10 +16,11 @@ export default function CurrentMembers() {
           <br/>
           Check our YouTube channel for application guidance. Interviews may be necessary, but officers will inform you in May or June if required this year.
         </p>
-        <button className={styles.renewalButton}>Renewal Application
-          <img className={styles.renewalButton} src="/page-icons/hyperlink.svg"/>
-        </button>
+        <a className={styles.link} href="https://forms.bestbuddies.org/4895599">Renewal Application
+          <img src="/page-icons/hyperlink.svg"/>
+        </a>
       </div>
+
       <div className={vidPlayerStyles.youtubeContainer}>
         <div className={vidPlayerStyles.youtubeCard}>
           <iframe
@@ -27,24 +28,22 @@ export default function CurrentMembers() {
             width="560" height="315"
             src="https://www.youtube-nocookie.com/embed/jpDCf0O0rsc?si=qUAJXnxWaqNqUjpF&amp;controls=0"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
       </div>
-
-
 
       <div className={styles.friendUpdatesBox}>
         <p className={styles.subPageTitle}>Friendship Updates!</p>
         <p className={styles.subPageContent}>
           Starting in November, every Buddy and Peer Buddy will need to complete a Friendship Update each month. These updates let us know how your friendship is going and if you need support.
         </p>
-
-        <button className={styles.friendUpdatesButton}>Friendship Updates
+        
+        <a className={styles.link} href="">Friendship Updates
           <img src="/page-icons/hyperlink.svg"/>
-        </button>
+        </a>
       </div>
 
       <div className={styles.latestNewsBox}>
@@ -54,35 +53,31 @@ export default function CurrentMembers() {
         </p>
 
         <div className={styles.prevNewsBox}>
-          <button className={styles.latestNewsButton}>Dec 2023
+          <a className={styles.currentMonthLink} href="/newsletter/newsletter_1.pdf" target="_blank">Dec 2023
             <img src="/page-icons/hyperlink.svg"/>
-
-          </button>
+          </a>
           <div className={styles.prevNewsImgContainer}>
-            <Image
+          <Image
                   className={styles.newsletterImg}
                   width={696}
                   height={483}
-                  src="/member-imgs/best_buddies_newsletter_img.png"
+                  src="/newsletter/newsletter_1.jpg"
+                  alt="Current Newsletter"
               />
             <div className={styles.pastNewsColBox}>
-              <h2>Past Newsletters</h2>
+              <h4 className={styles.pastNewsTitle}>Past Newsletters</h4>
               <div className={styles.prevNewsLinksContainer}>
-              <button className={styles.prevNewsLinks}>Oct 2023
+              <a className={styles.link} href="/newsletter/newsletter_2.pdf" target="_blank">Oct 2023
                 <img src="/page-icons/hyperlink.svg"/>
-              </button>
-              <button className={styles.prevNewsLinks}>Nov 2023
+              </a>
+              <a className={styles.link} href="/newsletter/newsletter_3.pdf" target="_blank">Nov 2023
                 <img src="/page-icons/hyperlink.svg"/>
-              </button>
+              </a>
               </div>
             </div>
           </div>
         </div>
-
-
-
       </div>
-
     </div>
   );
 }
