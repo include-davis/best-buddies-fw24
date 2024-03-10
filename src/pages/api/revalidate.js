fb24c139ea812071b57c6407721c649672531e66
Ryan <ryyu444@gmail.com>
2024-03-10 12:10:58 -0700

    fixed revalidate calls

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,13 +1,13 @@
 export default async function handler(_, res) {
   try {
     await res.revalidate("/");
-    await res.revalidate("/about");
     await res.revalidate("/about/ucd");
-    await res.revalidate("contact");
+    await res.revalidate("/about/intl");
     await res.revalidate("/events/chapter");
     await res.revalidate("/events/regional-state");
-    await res.revalidate("/members/current");
     await res.revalidate("/members/new");
+    await res.revalidate("/members/current");
+    // await res.revalidate("contact");
     return res.json({ ok: true, error: null });
   } catch (err) {
     return res.status(500).json({ ok: false, error: err });
