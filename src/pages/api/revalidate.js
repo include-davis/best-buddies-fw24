d84fcc027e1a666acdf7108b418f2841ee72ae09
Austin Shih <austinhshih@gmail.com>
2024-03-09 18:35:35 -0800

    Update revalidate.js

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,15 +1,15 @@
 export default async function handler(_, res) {
   try {
     await res.revalidate("/");
-    await res.revalidate("/about/intl");
-    await res.revalidate("/about/ucd");
-    await res.revalidate("/contact");
-    await res.revalidate("/events/chapter");
-    await res.revalidate("/events/regional-state");
-    await res.revalidate("/members/current");
-    await res.revalidate("/members/new");
-    return res.json({ revalidated: true });
+    // await res.revalidate("/about");
+    // await res.revalidate("/about/ucd");
+    // await res.revalidate("contact");
+    // await res.revalidate("/events/chapter");
+    // await res.revalidate("/events/regional-state");
+    // await res.revalidate("/members/current");
+    // await res.revalidate("/members/new");
+    return res.json({ ok: true, error: null });
   } catch (err) {
-    return res.status(500).send("Error revalidating");
+    return res.status(500).json({ ok: false, error: err });
   }
 }
