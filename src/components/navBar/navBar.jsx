5a3626ba60a9edc818a916ca407f9a0daecf93aa
Ryan <ryyu444@gmail.com>
2024-03-10 05:31:38 -0700

    partly done navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,11 +1,65 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Dropdown from "@/components/navbar/dropdown/dropdown";
+import AutoImage from "@/components/AutoImage/AutoImage";
+import navlinks from "@/data/navbar/navlinks";
 import styles from "@/styles/components/navbar/navbar.module.scss";
-// import Image from "next/image";
-// import Link from "next/link";
-// import Dropdown from "@/components/navbar/dropdown/dropdown";
-// import Button from "@/components/button/button";
-// import React, { useState, useEffect } from "react";
-// import AutoImage from "../AutoImage/AutoImage";
 
 export default function Navbar() {
-  return <nav className={styles.navbar}>navbar</nav>;
+  const [active, setActive] = useState(false);
+
+  const toggleMenu = () => {
+    setActive((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setActive(false);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <AutoImage src="/best-buddies-logo.svg" alt="Best Buddies Logo" />
+      </div>
+      <div className={styles.rightside}>
+        <ul className={styles.navlinks}>
+          {navlinks.map((link) => {
+            if (link.sublinks) {
+              return (
+                <Dropdown
+                  state={setActive}
+                  title={link.title}
+                  sublinks={link.sublinks}
+                  key={link.id}
+                  // styles={{ zindex: 100 }}
+                />
+              );
+            }
+
+            return (
+              <li className={styles.link} key={link.id} onClick={closeMenu}>
+                <Link className={`body-1-bold`} href={link.url}>
+                  {link.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.joinUs}>
+          <a href="">
+            <button className={styles.joinButton}>Join Us</button>
+          </a>
+        </div>
+        <div className={styles.mobileMenu} onClick={toggleMenu}>
+          <Image
+            src={active ? "page-icons/exit.svg" : "page-icons/menu.svg"}
+            width={active ? 20 : 24}
+            height={active ? 20 : 24}
+            alt={"mobile menu"}
+          />
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,13 +1,13 @@
 export default async function handler(_, res) {
   try {
     await res.revalidate("/");
-    // await res.revalidate("/about");
-    // await res.revalidate("/about/ucd");
-    // await res.revalidate("contact");
+    await res.revalidate("/about");
+    await res.revalidate("/about/ucd");
+    await res.revalidate("contact");
     await res.revalidate("/events/chapter");
-    // await res.revalidate("/events/regional-state");
-    // await res.revalidate("/members/current");
-    // await res.revalidate("/members/new");
+    await res.revalidate("/events/regional-state");
+    await res.revalidate("/members/current");
+    await res.revalidate("/members/new");
     return res.json({ ok: true, error: null });
   } catch (err) {
     return res.status(500).json({ ok: false, error: err });
