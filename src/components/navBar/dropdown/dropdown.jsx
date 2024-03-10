dc65b5ba5a817ccef5b512e0fedaf6327ae93d59
Austin Shih <austinhshih@gmail.com>
2024-03-09 22:10:08 -0800

    fixed eslint and node dependency

--- src/components/navBar/dropdown/dropdown.jsx
+++ src/components/navBar/dropdown/dropdown.jsx
@@ -1,7 +1,12 @@
 import styles from "@/styles/components/navbar/dropdown/dropdown.module.scss";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-export default function Dropdown({ children, state, setState, isMobile }) {
+export default function Dropdown({
+  children,
+  state: _,
+  setState: __,
+  isMobile,
+}) {
   return (
     <div
       className={isMobile ? `${styles.dropdownMobile}` : `${styles.dropdown}`}
