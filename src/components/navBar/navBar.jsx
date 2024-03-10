a00348f1e7f3e30adc8e16796ed30f7e9a6aac57
Ryan <ryyu444@gmail.com>
2024-03-10 00:29:33 -0800

    implemented cms to regional-state page

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,11 +1,11 @@
 import styles from "@/styles/components/navbar/navbar.module.scss";
-import Image from "next/image";
-import Link from "next/link";
-import Dropdown from "@/components/navBar/dropdown/dropdown";
-import Button from "@/components/button/button";
-import React, { useState, useEffect } from "react";
-import AutoImage from "../AutoImage/AutoImage";
+// import Image from "next/image";
+// import Link from "next/link";
+// import Dropdown from "@/components/navbar/dropdown/dropdown";
+// import Button from "@/components/button/button";
+// import React, { useState, useEffect } from "react";
+// import AutoImage from "../AutoImage/AutoImage";
 
 export default function Navbar() {
-  return <div></div>;
+  return <nav className={styles.navbar}>navbar</nav>;
 }
