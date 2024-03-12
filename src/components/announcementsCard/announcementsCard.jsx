a41f7b7383ca8b4f488b5c12fb4c625f861c1fb1
Ryan <ryyu444@gmail.com>
2024-03-11 21:55:53 -0700

    changed dates on announcement card

--- src/components/announcementsCard/announcementsCard.jsx
+++ src/components/announcementsCard/announcementsCard.jsx
@@ -2,6 +2,30 @@ import styles from "@/styles/components/announcementsCard/announcementsCard.modu
 import Link from "next/link";
 import Image from "next/image";
 
+function formatDateToMonthDayYear(date) {
+  date = new Date(date);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const month = months[date.getMonth()];
+  const day = date.getDate() + 1;
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
+
 export default function AnnouncementsCard({
   title,
   date,
@@ -12,7 +36,7 @@ export default function AnnouncementsCard({
     <div className={styles.announcementsCard}>
       <div className={styles.titleDateContainer}>
         <h4 className={`subheading`}>{title}</h4>
-        <p className={`body-2`}>{date}</p>
+        <p className={`body-2`}>{formatDateToMonthDayYear(date)}</p>
       </div>
       <p className={`body-1`}>{description}</p>
       {href && (
